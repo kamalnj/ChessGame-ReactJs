544b259d6a81d6c99d48096c73d121cb5d281c6d
kamalnj <99343671+kamalnj@users.noreply.github.com>
2024-01-31 21:07:13 +0100

    adding pieces

---
+++ src/components/Board/bits/Files.js
@@ -0,0 +1,11 @@
+import { getCharacter } from '../Helper'
+import './Files.css'
+
+
+const Files = ({files}) =>{
+    return (<div className="files">
+    {files.map(file=><span key={file}>{getCharacter(file)}</span>)}
+    </div>)
+    }
+
+export default Files
---
+++ src/components/Board/pieces/Piece.js
@@ -0,0 +1,11 @@
+const Piece = ({
+rank,
+file,
+piece,
+})=>{
+    return (
+    <div  className={`piece ${piece} p-${file}${rank}`}
+    draggable={true}
+    />)
+}
+export default Piece
