4b0c6636422b9a3c8378e675a55da4fcf1d23e5b
kamalnj <99343671+kamalnj@users.noreply.github.com>
2024-02-01 19:02:58 +0100

    Drraging pieces

--- src/components/Board/pieces/Piece.js
+++ src/components/Board/pieces/Piece.js
@@ -3,9 +3,23 @@ rank,
 file,
 piece,
 })=>{
+    const onDragStart = e =>{
+    e.dataTransfer.effectAllowed = 'move'
+    e.dataTransfer.setData('text/plain',`${piece},${rank},${file}`)
+    setTimeout(()=>{e.target.style.display = 'none'
+},0)
+    
+    }
+
+    const onDragEnd = e => e.target.style.display = 'block'
+
+
+
     return (
     <div  className={`piece ${piece} p-${file}${rank}`}
+    onDragEnd = {onDragEnd}
     draggable={true}
+    onDragStart={onDragStart}
     />)
 }
 export default Piece
--- src/components/Board/pieces/Pieces.js
+++ src/components/Board/pieces/Pieces.js
@@ -1,42 +1,49 @@
 import './Pieces.css'
 import Piece from './Piece'
+import { useState , useRef } from 'react';
+import { createPosition , copyPosition } from '../Helper';
+import { logDOM } from '@testing-library/react';
 
 const Pieces = () => {
-const position = new Array(8).fill('').map(x=>new Array(8).fill('')) 
-for(let i=0;i<8;i++){
-    position[1][i]='wp'
-    position[6][i]='bp'
+    const ref = useRef()
+    const [state, setstate] = useState(createPosition());
+    const calculateCoords = e => {
+        const{width,left,top} = ref.current.getBoundingClientRect()
+        const size = width / 8
+        const y = Math.floor((e.clientX - left)/size)
+        const x = 7 - Math.floor((e.clientY - top)/size)
+        return {x,y}
+    } 
+
+const onDrop = e =>{
+    const newPosition = copyPosition (state)
+    const {x,y} = calculateCoords(e)
+    const [p,rank,file]=e.dataTransfer.getData('text').split(',')
+    newPosition[rank][file] = ''
+    newPosition[x][y] = p
+    setstate(newPosition)
+
 }
-position[0][0]='wr'
-position[0][1]='wn'
-position[0][2]='wb'
-position[0][3]='wk'
-position[0][4]='wq'
-position[0][5]='wb'
-position[0][6]='wn'
-position[0][7]='wr'
 
-position[7][0]='br'
-position[7][1]='bn'
-position[7][2]='bb'
-position[7][3]='bk'
-position[7][4]='bq'
-position[7][5]='bb'
-position[7][6]='bn'
-position[7][7]='br'
-
-
+const onDragOver = e => e.preventDefault()
 
 return (
-    <div className='pieces'>
-        {position.map((r,rank)=>
+    
+    <div
+    ref = {ref}
+     onDrop={onDrop}
+     onDragOver={onDragOver}
+    
+    className='pieces'>
+
+        {state.map((r,rank)=>
         r.map((f,file)=>
-        position[rank][file]
+        state[rank][file]
         ?  <Piece
         key={rank+'-'+file}
            rank={rank}
            file={file}
-           piece={position[rank][file]}
+           piece={state[rank][file]}
         />
         :  null
         ))
